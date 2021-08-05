c4d362fc4fc318115694fbb7a89f62d52401786a
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2021-08-05 15:57:09 -0400

    add bottom menu in main component

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,5 +1,11 @@
 import styled, { css } from "styled-components";
-import { ArrowLeft } from "../../styles/Icons";
+import {
+  ArrowLeft,
+  Home,
+  Search,
+  Notifications,
+  Email,
+} from "../../styles/Icons";
 
 export const Container = styled.div`
   display: flex;
@@ -26,6 +32,8 @@ export const Header = styled.div`
     border-radius: 50%;
     outline: 0;
     cursor: pointer;
+    user-select: none;
+    -webkit-tap-highlight-color: transparent;
     &:hover {
       background: var(--twitter-dark-hover);
     }
@@ -50,4 +58,45 @@ export const ProfileInfo = styled.div`
     font-size: 15px;
     color: var(--gray);
   }
+`;
+
+export const BottomMenu = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+  background: var(--primary);
+  width: 100%;
+  border-top: 1px solid var(--outline);
+  display: flex;
+  justify-content: space-between;
+  padding: 8px min(46px, max(10vw, 10px));
+  @media (min-width: 500px) {
+    display: none;
+  }
+`;
+
+const iconCSS = css`
+  width: 31px;
+  height: 31px;
+  cursor: pointer;
+  fill: var(--gray);
+  -webkit-tap-highlight-color: transparent;
+  &:hover,
+  &.active {
+    fill: var(--twitter);
+  }
+`;
+
+export const HomeIcon = styled(Home)`
+  ${iconCSS}
+`;
+export const SearchIcon = styled(Search)`
+  ${iconCSS}
+`;
+export const BellIcon = styled(Notifications)`
+  ${iconCSS}
+`;
+export const EmailIcon = styled(Email)`
+  ${iconCSS}
 `;
