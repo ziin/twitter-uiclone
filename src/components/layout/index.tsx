46dda1c2b02be83bcfafb951809ba3b2d0972c33
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2021-08-06 16:50:15 -0400

    add sidebar

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,6 @@
 import Main from "../Main";
 import MenuBar from "../MenuBar";
+import SideBar from "../SideBar";
 
 import { Container, Wrapper } from "./styles";
 
@@ -9,7 +10,7 @@ const Layout = () => {
       <Wrapper>
         <MenuBar />
         <Main />
-        {/* <SideBar /> */}
+        <SideBar />
       </Wrapper>
     </Container>
   );
